3eaed65a0793c47ae2b51c14eb5d4fd2606c39c1
Nehemiah Dias <microsoftdavid4@gmail.com>
2022-08-11 10:34:33 -0700

    add basic React I/O

--- src/components/markupComponents/MarkupComponent.js
+++ src/components/markupComponents/MarkupComponent.js
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MarkupComponent.css';
 import { marked } from 'marked';
 
 function MarkupComponent() {
-  return (
-    <>
-        <div className='input'>
-            <div className='input-nav'>
-                <div className='title'>
-                    <p><i class="fa-solid fa-hashtag"></i>Editor</p>
+    const [input, setInput] = useState('');
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    return (
+        <>
+            <div className='input'>
+                <div className='input-nav'>
+                    <div className='title'>
+                        <p><i class="fa-solid fa-hashtag"></i>Editor</p>
+                    </div>
+                    <div className='expand'>
+                        <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                    </div>
                 </div>
-                <div className='expand'>
-                    <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                <textarea rows="10" value={input} onChange={handleChange}></textarea>
+            </div>
+            <div className='output'>
+                <div className='input-nav'>
+                    <div className='title'>
+                        <p><i class="fa-solid fa-hashtag"></i>Previewer</p>
+                    </div>
+                    <div className='expand'>
+                        <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                    </div>
+                </div>
+                <div className='output-area'>
+                    <div>{input}</div>
                 </div>
             </div>
-            <textarea rows="10"></textarea>
-        </div>
-        <div className='output'>
-            <div className='input-nav'>
-                <div className='title'>
-                    <p><i class="fa-solid fa-hashtag"></i>Previewer</p>
-                </div>
-                <div className='expand'>
-                    <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
-                </div>
-            </div>
-            <div className='output-area'>
-            </div>
-        </div>
-    </>
+        </>
   )
 }
 
